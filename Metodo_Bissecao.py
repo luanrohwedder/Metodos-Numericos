174b00eda4c9b3096ad4bf29fe185d6815b292f5
Luan Rohwedder <luan-rohwedder@gmail.com>
2021-08-18 03:13:14 -0400

    Método da Bissecção

---
+++ Metodo_Bissecao.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+def funcao(x):
+  return x**2-3
+
+def bissecao(a, b, ATOL):
+    if(funcao(a)*funcao(b) > 0):
+        print("Intervalo nao e valido!")
+    else:
+        n = np.ceil(np.log2((a+b)/(2*ATOL)))
+        iteracoes = n        
+        a_alg = a
+        b_alg = b
+
+        intervalo_a.append(a_alg)
+        intervalo_b.append(b_alg)
+      
+        for i in range(int(n)):
+            p = (a_alg + b_alg) / 2
+            fp = funcao(p)            
+            e = (b_alg - a_alg) / 2
+
+            raizes.append(round(p, 5))
+            funcao_raizes.append(round(fp, 5))
+            funcao_a.append(round(funcao(a_alg), 5))
+            funcao_b.append(round(funcao(b_alg), 5))
+            erro.append(round(e, 5))
+            
+            if funcao(a_alg) * fp < 0: 
+                b_alg = p                
+            else: 
+                a_alg = p
+                
+            intervalo_b.append(round(b_alg, 5))   
+            intervalo_a.append(round(a_alg, 5))
+            
+        return p
+
+#Listas
+iteracoes = 0
+raizes = []
+funcao_raizes = []
+funcao_a = []
+funcao_b = []
+intervalo_a = []
+intervalo_b = []
+erro = []
+
+#Insercao do intervalo
+print("Insira o intervalo:")
+print("a: ", end='')
+a = float(input())
+print("b: ", end='')
+b = float(input())
+
+#Insercao da aproximacao
+print("\nInsira a aproximacao:")
+epsilon = float(input())
+
+res = bissecao(a, b, epsilon)
+
+print()
+
+print('{:10}'.format(' n'), end='')
+print('{:10}'.format('a'), end='')
+print('{:10}'.format('b'), end='')
+print('{:8}'.format('x'), end='')
+print('{:10}'.format('f(a)'), end='')
+print('{:11}'.format('f(x)'), end='')
+print('{:10}'.format('f(b)'), end='')
+print('f(e)')
+
+
+for i in range(11):
+    print('{:2}'.format(i+1), end='')    
+    print('{0:10}'.format(intervalo_a[i]), end='')    
+    print('{0:10}'.format(intervalo_b[i]), end='')
+    print('{:10}'.format(raizes[i]), end='')
+    print('{:10}'.format(funcao_a[i]), end='')
+    print('{:10}'.format(funcao_raizes[i]), end='')
+    print('{:10}'.format(funcao_b[i]), end='')
+    print('{:10}'.format(erro[i]))    
